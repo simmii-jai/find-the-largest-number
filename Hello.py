815db04e7b88ec160aa68e2bb95b2d6182e918e1
simmii-jai <153538293+simmii-jai@users.noreply.github.com>
2023-12-12 13:21:21 +0000

    CODES UPDATED

--- Hello.py
+++ Hello.py
@@ -1,51 +1,20 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
 from streamlit.logger import get_logger
 
-LOGGER = get_logger(__name__)
+import streamlit as st
 
+st.title('FIND LARGEST AMONG GIVEN 3 NUMBERS')
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+st.header('Inputs: ')
 
-    st.write("# Welcome to Streamlit! 👋")
+number1 = st.number_input('Enter the First number')
+number2 = st.number_input('Enter the second number')
+number3 = st.number_input('Enter the Third number')
 
-    st.sidebar.success("Select a demo above.")
+def maximum():
+  lis = [number1 , number2 , number3]
+  a = max(lis)
+  st.success(f"The Largest number is {a}")
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
-
-if __name__ == "__main__":
-    run()
+if st.button("Find the Answer"):
+    maximum()
